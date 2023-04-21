4b54ebca5d4c6dc4369f21a0b4ea8ddf4eb8203f
Dominic Etienne Charrier <docharri@amd.com>
2023-04-21 11:35:53 +0200

    Define libclang dependency versions
    Build works with ``libclang`` >= 14.0.1, != 15.0.3, != 15.0.6, <= 15.0.6.1
    
    The following ``libclang`` versions have been tested:
    
    * 9.0.1	-- fail
    * 10.0.1.0 -- fail
    * 11.0.0 -- fail
    * 11.0.1 -- fail
    * 11.1.0 -- fail
    * 12.0.0 -- fail
    * 13.0.0 -- fail
    * 14.0.1 -- **success**
    * 14.0.6 -- **success**
    * 15.0.3 -- fail
    * 15.0.6 -- fail
    * 15.0.6.1 -- **success**
    * 16.0.0 -- fail

--- run_default_build.sh
+++ run_default_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+
 export ROCM_PATH=/opt/rocm # adjust accordingly
 export PYTHONPATH=$(pwd):${PYTHONPATH} # put local path into PYTHONPATH,
                                        # as `build` will copy only the setup script and package files into a temporary dir
-python3 -m build
+python3 -m build . "$@"
