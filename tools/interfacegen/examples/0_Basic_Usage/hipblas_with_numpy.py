13f76ca3dd7272486661915be17887148f02e588
domcharrier <docharri@amd.com>
2023-04-26 12:21:46 +0000

    Fix the hipblas + numpy example
    * Fixed various typos and issues in the draft.
    * Add result verification at the end of the file.

--- tools/interfacegen/examples/0_Basic_Usage/hipblas_with_numpy.py
+++ tools/interfacegen/examples/0_Basic_Usage/hipblas_with_numpy.py
@@ -9,24 +9,29 @@ def hip_check(call_result):
     if isinstance(call_result,tuple):
         err = call_result[0]
         result = call_result[1:]
+        if len(result) == 1:
+            result = result[0]
     else:
         err = call_result
         result = None
-    if isinstance(err,hip.hipStatus_t) and err != hip.hipStatus_t.hipSuccess:
+    if isinstance(err,hip.hipError_t) and err != hip.hipError_t.hipSuccess:
         raise RuntimeError(str(err))
     elif isinstance(err,hipblas.hipblasStatus_t) and err != hipblas.hipblasStatus_t.HIPBLAS_STATUS_SUCCESS:
         raise RuntimeError(str(err))
     return result
 
 num_elements = 100
-num_bytes = num_elements * np.dtype(np.float32).itemsize
 
 # input data on host
-alpha = ctypes.c_float(1.2)
-x_h = np.random.rand(num_bytes).astype(dtype=np.float32)
-y_h = np.random.rand(num_bytes).astype(dtype=np.float32)
+alpha = ctypes.c_float(2)
+x_h = np.random.rand(num_elements).astype(dtype=np.float32)
+y_h = np.random.rand(num_elements).astype(dtype=np.float32)
+
+# expected result
+y_expected = alpha*x_h + y_h
 
 # device vectors
+num_bytes = num_elements * np.dtype(np.float32).itemsize
 x_d = hip_check(hip.hipMalloc(num_bytes))
 y_d = hip_check(hip.hipMalloc(num_bytes))
 
@@ -35,19 +40,21 @@ hip_check(hip.hipMemcpy(x_d,x_h,num_bytes,hip.hipMemcpyKind.hipMemcpyHostToDevic
 hip_check(hip.hipMemcpy(y_d,y_h,num_bytes,hip.hipMemcpyKind.hipMemcpyHostToDevice))
 
 # call hipblasSaxpy + initialization & destruction of handle
-handle = hip_check(hipblas.hipblasHandleCreate())
-hip_check(hipblas.hipblasSaxpy(handle, num_elements, ctypes.addressof(alpha), x_d, 0, y_d, 0))
+handle = hip_check(hipblas.hipblasCreate())
+hip_check(hipblas.hipblasSaxpy(handle, num_elements, ctypes.addressof(alpha), x_d, 1, y_d, 1))
 hip_check(hipblas.hipblasDestroy(handle))
 
 # copy result (stored in y_d) back to host (store in y_h)
 hip_check(hip.hipMemcpy(y_h,y_d,num_bytes,hip.hipMemcpyKind.hipMemcpyDeviceToHost))
 
 # compare to expected result
-y_expected = alpha*x_h + y_h
-print(f"y_h == y_expected: {np.allclose(y_expected,x_h)}")
+if np.allclose(y_expected,y_h):
+    print("ok")
+else:
+    print("FAILED")
+#print(f"{y_h=}")
+#print(f"{y_expected=}")
 
 # clean up
 hip_check(hip.hipFree(x_d))
 hip_check(hip.hipFree(y_d))
-
-
