97155299cdfadc2091397f8c38f4e865874ff4f4
Dominic Etienne Charrier <docharri@amd.com>
2023-06-20 17:22:01 +0200

    feat: Make helper scripts non-sourceable

--- tools/interfacegen/generate_hip_python_pkgs.sh
+++ tools/interfacegen/generate_hip_python_pkgs.sh
@@ -1,11 +1,49 @@
 #!/usr/bin/env bash
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+   echo "ERROR: script must not be sourced";
+   return 1
+fi
 
-[ ! -d "venv" ] && python3 -m venv venv
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -b|--pre-clean)
+      PRE_CLEAN=1
+      shift
+      ;;
+    -a|--post-clean)
+      POST_CLEAN=1
+      shift
+      ;;
+    -n|--no-venv)
+      NO_VENV=1
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
 
-  venv/bin/python3 -m pip install -r requirements.txt
+[ -z ${PRE_CLEAN+x} ] || rm -rf venv
 
-  HIP_PLATFORM=amd \
-  HIP_PYTHON_LIBS=* \
-  ROCM_PATH=/opt/rocm \
-  HIP_PYTHON_CLANG_RES_DIR=$(${ROCM_PATH}/llvm/bin/clang -print-resource-dir) \
-  venv/bin/python3 codegen_hip_python.py
+alias PYTHON="python3"
+if [ -z ${NO_ENV+x} ]; then
+  [ ! -d "venv" ] && python3 -m venv venv
+  alias PYTHON="venv/bin/python3"
+fi
+shopt -s expand_aliases
+
+PYTHON -m pip install -r requirements.txt
+
+HIP_PLATFORM=amd \
+HIP_PYTHON_LIBS=* \
+ROCM_PATH=/opt/rocm \
+HIP_PYTHON_CLANG_RES_DIR=$(${ROCM_PATH}/llvm/bin/clang -print-resource-dir) \
+PYTHON codegen_hip_python.py
+
+[ -z ${POST_CLEAN+x} ] || rm -rf venv
