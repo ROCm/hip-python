e480c1594b38a9bf4f44018947534e2a47d58604
Dominic Etienne Charrier <docharri@amd.com>
2023-07-12 12:44:56 +0200

    readd readthedocs config

--- hip-python/docs/conf.py
+++ hip-python/docs/conf.py
@@ -8,91 +8,88 @@
 
 # requires: pip install rocm-docs myst-parser
 
-import os
-import sys
+from rocm_docs import ROCmDocs
 
-project = 'HIP Python'
-copyright = 'Copyright (c) 2023 Advanced Micro Devices, Inc.'
-author = 'Advanced Micro Devices, Inc. <hip-python.maintainer@amd.com>'
+
+project = "HIP Python"
+author = "Advanced Micro Devices, Inc. <hip-python.maintainer@amd.com>"
+copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc."
+# article info
 os_support = ["linux"]
 date = "2023-06-23"
 
-default_role = "py:obj" # this means that `test` will be expanded to :py:obj`test`
-
-extensions = [
-  "sphinx.ext.autodoc",  # Automatically create API documentation from Python docstrings
-  "myst_parser", # Allows to embed reST code in Markdown code
-]
+default_role = "py:obj"  # this means that `test` will be expanded to :py:obj`test`
 
 # NOTE: always install the HIP Python packages, do not add the source folders
-# to the sys path, i.e. do not add .. and ../hip-python-as-cuda as 
+# to the sys path, i.e. do not add .. and ../hip-python-as-cuda as
 # this breaks autodoc's automodule routine.
 
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'undoc-members': True,
-    'special-members': '__init__, __getitem__',
-    'inherited-members': True,
-    'show-inheritance': True,
-    'imported-members': False,
-    'member-order': 'bysource', # bysource: seems unfortunately not to work for Cython modules
+    "members": True,
+    "member-order": "bysource",
+    "undoc-members": True,
+    "special-members": "__init__, __getitem__",
+    "inherited-members": True,
+    "show-inheritance": True,
+    "imported-members": False,
+    "member-order": "bysource",  # bysource: seems unfortunately not to work for Cython modules
 }
 
 # Rocm-docs-core
-from rocm_docs import ROCmDocs
 external_projects_remote_repository = ""
-external_projects_current_project = project
+external_projects_current_project = "hip"
 
 article_pages = [
     {
-      "file": "index",
-      "os": os_support,
-      "author": author,
-      "date": date,
-      "read-time": "1 min read",
+        "file": "index",
+        "os": os_support,
+        "author": author,
+        "date": date,
+        "read-time": "1 min read",
     },
     {
-      "file": "user_guide/0_install",
-      "os": os_support,
-      "author": author,
-      "date": date,
-      "read-time": "5 min read",
+        "file": "user_guide/0_install",
+        "os": os_support,
+        "author": author,
+        "date": date,
+        "read-time": "5 min read",
     },
     {
-      "file": "user_guide/1_usage",
-      "os": os_support,
-      "author": author,
-      "date": date,
-      "read-time": "60 min read",
+        "file": "user_guide/1_usage",
+        "os": os_support,
+        "author": author,
+        "date": date,
+        "read-time": "60 min read",
     },
     {
-      "file": "user_guide/2_cuda_python_interop",
-      "os": os_support,
-      "author": author,
-      "date": date,
-      "read-time": "20 min read",
+        "file": "user_guide/2_cuda_python_interop",
+        "os": os_support,
+        "author": author,
+        "date": date,
+        "read-time": "20 min read",
     },
     {
-      "file": "user_guide/3_datatypes",
-      "os": os_support,
-      "author": author,
-      "date": date,
-      "read-time": "10 min read",
+        "file": "user_guide/3_datatypes",
+        "os": os_support,
+        "author": author,
+        "date": date,
+        "read-time": "10 min read",
     },
     {
-      "file": "user_guide/4_report_bugs",
-      "os": os_support,
-      "author": author,
-      "date": date,
-      "read-time": "20 min read",
+        "file": "user_guide/4_report_bugs",
+        "os": os_support,
+        "author": author,
+        "date": date,
+        "read-time": "20 min read",
     },
 ]
-
-#html_extra_path = ["_static"]
 
 docs_core = ROCmDocs(project)
 docs_core.setup()
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
+
+extensions += [
+    "sphinx.ext.autodoc",  # Automatically create API documentation from Python docstrings
+]
