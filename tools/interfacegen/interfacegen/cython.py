1cfa01fed6809d3931de1f59e3214302203381ba
domcharrier <docharri@amd.com>
2023-10-27 23:21:02 +0000

    docs(i*gen.cython): list undocumented out args in returns section

--- tools/interfacegen/interfacegen/cython.py
+++ tools/interfacegen/interfacegen/cython.py
@@ -1645,7 +1645,10 @@ cdef void* {funptr_name} = NULL
                 _log.warn(f"<{self.render_location()}> function {self.name}: doxygen: function arg '{name}' is not documented.")
                 type_info = "/".join([CythonMixin.to_sphinx_pyobj(p) for p in parm_python_types[name].split("/")])
                 type_info = f" ({type_info})"
-                docstring_args[name] = (name+type_info,"",f"\n{single_level_indent*2}(undocumented)\n")
+                if name in out_arg_names:
+                    docstring_out_arg_returns.append(f"{single_level_indent}{type_info}:\n{single_level_indent}(undocumented)")
+                else:
+                    docstring_args[name] = (name+type_info,"",f"\n{single_level_indent*2}(undocumented)\n")
         # now generate the arguments
         if len(docstring_args):
             docstring_body += "\nArgs:\n"
