877af1dcc3a1b7c6d0043e763a631efb1b3f4758
domcharrier <docharri@amd.com>
2023-12-22 18:23:04 +0000

    remove whitespace

--- tools/interfacegen/recipes/python_cython/hip/ci/2-generate-code.sh
+++ tools/interfacegen/recipes/python_cython/hip/ci/2-generate-code.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 # MIT License
-# 
+#
 # Copyright (c) 2023 Advanced Micro Devices, Inc.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,7 +32,7 @@ set -e
 set -o xtrace
 
 if [ -z ${ROCM_VER+x} ]; then
-   echo "ERROR: environment variable 'ROCM_VER' not set."
+  echo "ERROR: environment variable 'ROCM_VER' not set."
   return 1
 fi
 
