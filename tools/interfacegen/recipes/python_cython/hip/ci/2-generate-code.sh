96280af2c65113e0bbe42b51b506bb103e0afc7e
domcharrier <docharri@amd.com>
2023-12-29 12:56:59 +0000

    ci(**/2-generate-code.sh): add RELEASE_REPO_DIR default

--- tools/interfacegen/recipes/python_cython/hip/ci/2-generate-code.sh
+++ tools/interfacegen/recipes/python_cython/hip/ci/2-generate-code.sh
@@ -39,5 +39,6 @@ fi
 sudo apt install -y python3 python3-venv
 
 # generate code
+RELEASE_REPO_DIR=${RELEASE_REPO_DIR:-hip-python-release-repo}
 export PYTHONPATH=$(git rev-parse --show-toplevel)
 bash ./generate_hip_python_pkgs.sh ${RELEASE_REPO_DIR} --rocm-version ${ROCM_VER}
