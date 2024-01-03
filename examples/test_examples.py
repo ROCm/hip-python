3a024ce54c9f015ae9badd2dc22bf3da9e372a6d
domcharrier <docharri@amd.com>
2024-01-03 16:37:17 +0000

    fix(examples): disable hiprtc tests for rocm 5.5

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,9 +1,34 @@
+# MIT License
+#
+# Copyright (c) 2023 Advanced Micro Devices, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import runpy
 import os
 import subprocess
 
 import pytest
 import shlex
+
+from hip import ROCM_VERSION_TUPLE
+enable_hiprtc_tests = ROCM_VERSION_TUPLE[0:2] != (5,5)
 
 try:
     from cuda import cuda
@@ -20,10 +45,14 @@ python_examples = [
   "0_Basic_Usage/hipblas_with_numpy.py",
   "0_Basic_Usage/hipfft.py",
   "0_Basic_Usage/hiprand_monte_carlo_pi.py",
-  "0_Basic_Usage/hiprtc_launch_kernel_args.py",
-  "0_Basic_Usage/hiprtc_launch_kernel_no_args.py",
   "0_Basic_Usage/rccl_comminitall_bcast.py",
 ]
+
+if enable_hiprtc_tests:
+    python_examples += [
+      "0_Basic_Usage/hiprtc_launch_kernel_args.py",
+      "0_Basic_Usage/hiprtc_launch_kernel_no_args.py",
+    ]
 
 if have_hip_python_as_cuda:
     python_examples += [
@@ -45,3 +74,4 @@ if have_hip_python_as_cuda:
     def test_cython_examples(example):
         abspath = os.path.join(os.path.dirname(__file__),os.path.dirname(example))
         subprocess.check_call(shlex.split(f"make -C {abspath} run"))
+
