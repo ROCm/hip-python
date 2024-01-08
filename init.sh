d0ce5c09cce5737fa0d60f0be72304e0a391550d
domcharrier <docharri@amd.com>
2024-01-08 07:13:30 -0800

    init.sh: make branch configurable

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+MAIN_BRANCH=${MAIN_BRANCH:-"origin/new_main"}
 git fetch origin
-git checkout origin/new_main -- ci
-git checkout origin/new_main -- build_hip_python_pkgs.sh
-git checkout origin/new_main -- _render_update_version.py
-git checkout origin/new_main -- docs
-git checkout origin/new_main -- examples
+git checkout ${MAIN_BRANCH} -- ci
+git checkout ${MAIN_BRANCH} -- build_hip_python_pkgs.sh
+git checkout ${MAIN_BRANCH} -- _render_update_version.py
+git checkout ${MAIN_BRANCH} -- docs
+git checkout ${MAIN_BRANCH} -- examples
 # unstage
 git reset -q HEAD ci build_hip_python_pkgs.sh _render_update_version.py docs examples
