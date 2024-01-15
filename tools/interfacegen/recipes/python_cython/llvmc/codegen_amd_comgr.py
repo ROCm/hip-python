f6cc0be265113207781ea4adb1d1b9e8b84a32f9
domcharrier <docharri@amd.com>
2024-01-15 14:27:39 +0000

    tune amd_comgr interfaces
    * Make them nogil and specify a return value that
      should trigger Python exception checks.
    * Add artificial success status to certain interfaces.
    * Specify more parameters as output values.

--- tools/interfacegen/recipes/python_cython/llvmc/codegen_amd_comgr.py
+++ tools/interfacegen/recipes/python_cython/llvmc/codegen_amd_comgr.py
@@ -46,14 +46,9 @@ interfacegen.cython.python_interface_pyobj_role_template = (
 
 from interfacegen.cython import (
     CythonModuleGenerator,
-    CREATE_DEFAULT_PTR_COMPLICATED_TYPE_HANDLER,
 )
 
-from interfacegen.tree import (
-    Node,
-    Parm,
-    Typed
-)
+from interfacegen.tree import Node, Parm, Typed
 
 from interfacegen.control import ParmIntent
 
@@ -61,7 +56,7 @@ from interfacegen.support import cython as support
 
 
 def create_generator(
-    global_module_name: str, header_file: str, **opts
+    global_module_name: str, header_file: str
 ) -> CythonModuleGenerator:
     global pkg_opts
 
@@ -74,19 +69,59 @@ def create_generator(
         func_name, parm_index = parm.parent.name, parm.index
         if func_name in (
             "amd_comgr_get_isa_count",
-            "amd_comgr_get_isa_name", # char** isa_name
-            # "amd_comgr_action_data_count",
             "amd_comgr_get_version",
-        ): 
-            return ParmIntent.OUT
-        if (func_name, parm_index) in (
-            ("amd_comgr_status_string",1),
-            ("amd_comgr_action_info_get_option_list_count",1),
+            "amd_comgr_create_data_set",
+            "amd_comgr_create_action_info",
         ):
             return ParmIntent.OUT
-        if parm.is_pointer_to_enum(degree=1):
+        if (func_name, parm_index) in (
+            ("comgr.amd_comgr_create_data", 1),
+            ("amd_comgr_status_string", 1),
+            ("amd_comgr_action_info_get_option_list_count", 1),
+            ("amd_comgr_create_data", 1),
+            ("amd_comgr_get_data_kind", 1),
+            ("amd_comgr_action_data_count", 2),
+            ("amd_comgr_action_data_get_data", 3),
+            ("amd_comgr_get_isa_metadata", 1),
+            ("amd_comgr_get_data_metadata", 1),
+            ("amd_comgr_get_metadata_kind", 1),
+            ("amd_comgr_get_metadata_map_size", 1),
+            ("amd_comgr_metadata_lookup", 2),
+            ("amd_comgr_get_metadata_list_size", 1),
+            ("amd_comgr_index_list_metadata", 2),
+        ):
             return ParmIntent.OUT
+        # INOUT:
+        # amd_comgr_get_data
+        # amd_comgr_get_data_name
+        # amd_comgr_get_isa_name
+        # amd_comgr_get_metadata_string
+        # amd_comgr_iterate_map_metadata
         return ParmIntent.INOUT
+
+    def node_filter(node: Node):
+        return (
+            node.global_name("_").startswith(
+                "amd_comgr"
+            )  # use global_name because of anonymous funptrs
+            or node.name.startswith("AMD_COMGR_INTERFACE_VERSION")
+            or node.name == "code_object_info_s"
+        )
+
+    def node_init(node: Node):
+        # node modifications
+        if isinstance(node, interfacegen.tree.Function):
+            if not node.is_enum and node.name.startswith("amd_comgr"):
+                # hip routines without hipError_t return status
+                # we force them to not throw exceptions
+                node.error_return_value_lazy_loader = None
+                node.modifiers_lazy_loader = " noexcept nogil"
+                # we force them to have always return hipSuccess as first return value
+                node.prepend_python_return_value(
+                    "amd_comgr_status_s.AMD_COMGR_STATUS_SUCCESS",
+                    "amd_comgr_status_s",
+                    "Always returns `~.amd_comgr_status_s.AMD_COMGR_STATUS_SUCCESS`.",
+                )
 
     generator = CythonModuleGenerator(
         global_module_name,
@@ -96,9 +131,12 @@ def create_generator(
         util_pkg=pkg_opts.util_pkg,
         dll=pkg_opts.dll,
         cflags=pkg_opts.generator_args,
+        node_filter=node_filter,
+        node_init=node_init,
         ptr_rank=ptr_rank,
         ptr_parm_intent=ptr_parm_intent,
-        **opts,
+        modifiers_lazy_loader=" except? AMD_COMGR_STATUS_ERROR nogil",
+        error_return_value_lazy_loader="AMD_COMGR_STATUS_ERROR",
     )
     # generator.c_interface_decl_prolog += cython_c_preamble
     generator.python_interface_decl_prolog += f"cimport {pkg_opts.util_pkg}.types\n"
@@ -108,26 +146,16 @@ def create_generator(
 
 def create_generators(root: it.Root):
     for node in root.walk_files():
-
-        def node_filter(node: Node):
-            return (
-                node.global_name("_").startswith(
-                    "amd_comgr"
-                )  # use global_name because of anonymous funptrs
-                or node.name.startswith("AMD_COMGR_INTERFACE_VERSION")
-                or node.name == "code_object_info_s"
-            )
-
         generator: CythonModuleGenerator = create_generator(
-            node.py_global_name, os.path.join("amd_comgr",node.relpath), node_filter=node_filter
+            node.py_global_name,
+            os.path.join("amd_comgr", node.relpath),
         )
-
         node.codegen = generator
 
 
 if __name__ == "__main__":
     interfacegen.cython.FunctionMixin.python_interface_always_return_tuple = (
-        False  # same for all modules
+        True
     )
 
     pkg_opts: support.RocmPackageOpts = support.create_rocm_package_opts_from_cli(
@@ -142,15 +170,21 @@ if __name__ == "__main__":
         email="hip-python.maintainer@amd.com",
     )
 
-    INCTREE = it.build_include_tree(
-        pkg_opts.abs_inc_dir,
-        py_namespace="rocm",  # namespace influences py package dirs
-        glob_expr=os.path.join("**", "amd_comgr", "*.h"),
-    ).find_node(py_global_name="rocm.amd_comgr").create_root( # rocm.amd_comgr: corresponds to /opt/rocm/include/amd_comgr
-        py_namespace="rocm.amd_comgr" # py output will be generated into "<package_dir>/rocm/amd_comgr/<py_mod_or_pkg_path>"
-    ) # make the node corresponding to "/opt/rocm/include/amd_comgr" the new root
+    INCTREE = (
+        it.build_include_tree(
+            pkg_opts.abs_inc_dir,
+            py_namespace="rocm",  # namespace influences py package dirs
+            glob_expr=os.path.join("**", "amd_comgr", "*.h"),
+        )
+        .find_node(py_global_name="rocm.amd_comgr")
+        .create_root(  # rocm.amd_comgr: corresponds to /opt/rocm/include/amd_comgr
+            py_namespace="rocm.amd_comgr"  # py output will be generated into "<package_dir>/rocm/amd_comgr/<py_mod_or_pkg_path>"
+        )
+    )  # make the node corresponding to "/opt/rocm/include/amd_comgr" the new root
     create_generators(INCTREE)
 
     support.generate_all_rocm_package_files(
-        INCTREE, pkg_opts, main_dir=os.path.join(pkg_opts.package_dir, "rocm","amd_comgr")
+        INCTREE,
+        pkg_opts,
+        main_dir=os.path.join(pkg_opts.package_dir, "rocm", "amd_comgr"),
     )
