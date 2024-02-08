667092b0def4e2e142d897b490ccd1bce67af16b
domcharrier <docharri@amd.com>
2024-02-08 15:52:51 +0000

    llvmutils: add IR/BC+ to IR/BC linker
    * This allows to link in Numba-HIP-specific
      bc/ir libraries before handing the result
      to another Python framework that works with HIPRCT / COMGR
      internally.

--- packages/numba-hip/numba/hip/util/llvmutils.py
+++ packages/numba-hip/numba/hip/util/llvmutils.py
@@ -40,10 +40,13 @@ from rocm.llvm.c.core import (
     LLVMDisposeMessage,
     LLVMContextCreate,
     LLVMContextDispose,
+    LLVMCloneModule,
+    LLVMModuleCreateWithName,
 )
 from rocm.llvm.c.bitreader import LLVMParseBitcode
 from rocm.llvm.c.bitwriter import LLVMWriteBitcodeToMemoryBuffer
 from rocm.llvm.c.irreader import LLVMParseIRInContext
+from rocm.llvm.c.linker import LLVMLinkModules2
 
 
 def llvm_check(status, message):
@@ -270,10 +273,140 @@ def convert_to_llvm_bc(buf, buf_len: int = -1):
     return result
 
 
+def _get_module(ir, ir_len: int = -1):
+    """Load LLVM module from human-readable LLVM IR or LLVM bitcode/
+
+    Args:
+        ir (UTF-8 `str`, or implementor of the Python buffer protocol such as `bytes`):
+            Buffer that contains LLVM IR or LLVM BC.
+        ir_len (`int`, optional):
+            Length of the LLVM IR/BC buffer. Must be supplied if it cannot
+            be obtained via ``len(buf)``.
+    Returns:
+        `tuple`:
+            A 3-`tuple` ``(mod, bc_buf, context)`` that contains (in that order):
+
+            * mod - The loaded LLVM module.
+            * bc_buf - A bitcode buffer, or None.
+            * context - A LLVM context that was used for parsing IR, or None.
+    """
+    (parse_ir_status, mod, msg, ir_buf, context) = _parse_llvm_ir(ir, ir_len)
+    if parse_ir_status > 0:  # failure
+        LLVMDisposeMessage(msg)
+        LLVMDisposeModule(mod)
+        # LLVMDisposeMemoryBuffer(ir_buf) # TODO LLVMParseIRInContext seems to take ownership of the buffer and its deletion, memory analysis needed
+        (parse_bc_status, mod, msg, bc_buf) = _parse_llvm_bc(ir, ir_len)
+        if parse_bc_status:
+            if msg:
+                LLVMDisposeMessage(msg)
+            raise ValueError(
+                f"input 'buf' seems to be neither LLVM bitcode nor human-readable LLVM IR"
+            )
+    else:
+        bc_buf = None
+    return (mod, bc_buf, context)
+
+
+def _get_module_dispose_all(mod, bc_buf, context):
+    """Clean up the results of `_get_module`.
+
+    Args:
+        mod: A module.
+        bc_buf: A bitcode buffer or None.
+        context: A LLVM context that was used for parsing IR, or None.
+    """
+    LLVMDisposeModule(mod)
+    if context:  # implies input is an IR module
+        # LLVMDisposeMemoryBuffer(ir_buf) # TODO LLVMParseIRInContext seems to take ownership of the buffer and its deletion, memory analysis needed
+        LLVMContextDispose(context)
+    if bc_buf:  # implies input is an BC module
+        LLVMDisposeMemoryBuffer(bc_buf)
+
+
+def _print_module(mod):
+    """Print llvm module to IR; mainly for debugging" """
+    msg = LLVMPrintModuleToString(mod)
+    print(msg)
+    LLVMDisposeMessage(msg)
+
+
+def link_modules(modules, to_bc: bool = False):
+    """Links the LLVM modules in the list together.
+
+    Note:
+        The result of this operation is order dependent.
+        In this implementation, we create an empty module
+        and then link it with ``modules[-1]`, ``modules[-2]``,
+        ... ``modules[0]``, i.e. the specified modules are
+        linked in reverse order.
+
+    Args:
+        modules (`iterable`):
+            The modules to link. The output
+            A list that contains entries of the following kind:
+
+            1. ir (UTF-8 `str`, or implementor of the Python buffer protocol such as `bytes`):
+                Buffer that contains LLVM IR or LLVM BC.
+                Buffer size must be obtainable via `len(...)`.
+            2. or a `tuple` that contains:
+              * ir (UTF-8 `str`, or implementor of the Python buffer protocol such as `bytes`):
+                  Buffer that contains LLVM IR or LLVM BC.
+              * ir_len (`int`):
+                  Length of the LLVM IR/BC buffer. Must be supplied if it cannot
+                  be obtained via ``len(buf)``.
+
+            Argument 1 implies that the buffer
+        to_bc (`bool`, optional):
+            If the result should be LLVM bitcode instead of human-readable LLVM IR.
+            Defaults to `False`.
+    """
+    if not len(modules):
+        raise ValueError("argument 'irs' must have at least one entry")
+    # create LLVM module from every input
+    cloned_modules = []
+    for entry in modules:
+        if isinstance(entry, tuple):
+            ir = entry[0]
+            ir_len = entry[1]
+        else:
+            ir = entry
+            ir_len = len(entry)
+        (mod, bc_buf, context) = _get_module(ir, ir_len)
+        cloned_modules.append(
+            (LLVMCloneModule(mod), (mod, bc_buf, context))
+        )  # store the result of _get_module to dispose later
+
+    # LLVMLinkModules2(Dest, Src) "Links the source module into the destination module. The source module is destroyed."
+    dest = LLVMModuleCreateWithName(b"link-modules-result")
+    for src in reversed(cloned_modules):
+        if LLVMLinkModules2(dest, src[0]) > 0:
+            raise RuntimeError("An error has occurred")
+    result = None
+    if to_bc:
+        bc_buf = LLVMWriteBitcodeToMemoryBuffer(dest)
+        bc_buf_len = LLVMGetBufferSize(bc_buf)
+        bc_ndbuffer = LLVMGetBufferStart(bc_buf).configure(
+            _force=True, shape=(bc_buf_len,)
+        )
+        result = copy.deepcopy(bytes(bc_ndbuffer))  # copies into new buffer
+        LLVMDisposeMemoryBuffer(bc_buf)
+    else:
+        ir = LLVMPrintModuleToString(dest)
+        result = copy.deepcopy(bytes(ir))  # copies into new buffer
+        LLVMDisposeMessage(ir)
+    # clean up
+    LLVMDisposeModule(dest)
+    for _, other in cloned_modules[:]:
+        # the cloned modules have been consumed by the linker
+        _get_module_dispose_all(*other)
+    return result
+
+
 if __name__ in ("__main__",):
     # TODO move into unit test
     import textwrap
 
+    # Test 1
     llvm_ir = textwrap.dedent(
         """\
     ; ModuleID = 'llvm-ir-buffer'
@@ -288,5 +421,97 @@ if __name__ in ("__main__",):
 
     print(convert_to_llvm_ir(llvm_ir))
     print(convert_to_llvm_ir(llvm_bc))
+
     print(convert_to_llvm_bc(llvm_ir))
     print(convert_to_llvm_bc(llvm_bc))
+
+    # Test 2
+    main_llvm_ir = textwrap.dedent(
+        # ```
+        # // main.hip:
+        # include "hip/hip_runtime.h"
+        #
+        # __device__ void scale(float* arr, float scal);
+        #
+        # __global__ void mykernel(float* arr, float scal) {
+        #    scale(arr,scal);
+        # }
+        # ```
+        # hipcc -S -emit-llvm main.hip
+        """\
+        ; ModuleID = 'main.hip'
+        source_filename = "main.hip"
+        target datalayout = "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7"
+        target triple = "amdgcn-amd-amdhsa"
+
+        ; Function Attrs: convergent mustprogress norecurse nounwind
+        define protected amdgpu_kernel void @_Z8mykernelPff(ptr addrspace(1) %0, float %1) local_unnamed_addr #0 {
+        %3 = addrspacecast ptr addrspace(1) %0 to ptr
+        tail call void @_Z5scalePff(ptr %3, float %1) #2
+        ret void
+        }
+
+        ; Function Attrs: convergent nounwind
+        declare hidden void @_Z5scalePff(ptr, float) local_unnamed_addr #1
+
+        attributes #0 = { convergent mustprogress norecurse nounwind "amdgpu-flat-work-group-size"="1,1024" "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="gfx90a" "target-features"="+16-bit-insts,+atomic-buffer-global-pk-add-f16-insts,+atomic-fadd-rtn-insts,+ci-insts,+dl-insts,+dot1-insts,+dot10-insts,+dot2-insts,+dot3-insts,+dot4-insts,+dot5-insts,+dot6-insts,+dot7-insts,+dpp,+gfx8-insts,+gfx9-insts,+gfx90a-insts,+mai-insts,+s-memrealtime,+s-memtime-inst,+wavefrontsize64" "uniform-work-group-size"="true" }
+        attributes #1 = { convergent nounwind "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="gfx90a" "target-features"="+16-bit-insts,+atomic-buffer-global-pk-add-f16-insts,+atomic-fadd-rtn-insts,+ci-insts,+dl-insts,+dot1-insts,+dot10-insts,+dot2-insts,+dot3-insts,+dot4-insts,+dot5-insts,+dot6-insts,+dot7-insts,+dpp,+gfx8-insts,+gfx9-insts,+gfx90a-insts,+mai-insts,+s-memrealtime,+s-memtime-inst,+wavefrontsize64" }
+        attributes #2 = { convergent nounwind }
+
+        !llvm.module.flags = !{!0, !1, !2, !3, !4}
+        !opencl.ocl.version = !{!5}
+        !llvm.ident = !{!6}
+
+        !0 = !{i32 4, !"amdgpu_hostcall", i32 1}
+        !1 = !{i32 1, !"amdgpu_code_object_version", i32 500}
+        !2 = !{i32 1, !"amdgpu_printf_kind", !"hostcall"}
+        !3 = !{i32 1, !"wchar_size", i32 4}
+        !4 = !{i32 8, !"PIC Level", i32 2}
+        !5 = !{i32 2, i32 0}
+        !6 = !{!"AMD clang version 17.0.0 (https://github.com/RadeonOpenCompute/llvm-project roc-6.0.0 23483 7208e8d15fbf218deb74483ea8c549c67ca4985e)"}
+        """
+    )
+
+    dep_llvm_ir = textwrap.dedent(
+        # LLVM IR extracted from
+        # hipcc -S -emit-llvm -fgpu-rdc dep.cpp
+        # ```
+        # // dep.cpp:
+        # #include "hip/hip_runtime.h"
+        # __device__ void scale_op(float arr[], float factor) {
+        #    arr[threadIdx.x] *= factor;
+        # }
+        # ```
+        """\
+        ; ModuleID = 'dep.hip'
+        source_filename = "dep.hip"
+        target datalayout = "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7"
+        target triple = "amdgcn-amd-amdhsa"
+
+        ; Function Attrs: mustprogress nofree nosync nounwind willreturn memory(argmem: readwrite)
+        define hidden void @_Z5scalePff(ptr nocapture %0, float %1) local_unnamed_addr #0 {
+            %3 = tail call i32 @llvm.amdgcn.workitem.id.x(), !range !0, !noundef !1
+            %4 = zext i32 %3 to i64
+            %5 = getelementptr inbounds float, ptr %0, i64 %4
+            %6 = load float, ptr %5, align 4, !tbaa !2
+            %7 = fmul contract float %6, %1
+            store float %7, ptr %5, align 4, !tbaa !2
+            ret void
+        }
+
+        ; Function Attrs: nocallback nofree nosync nounwind speculatable willreturn memory(none)
+        declare i32 @llvm.amdgcn.workitem.id.x() #1
+
+        attributes #0 = { mustprogress nofree nosync nounwind willreturn memory(argmem: readwrite) "no-trapping-math"="true" "stack-protector-buffer-size"="8" "target-cpu"="gfx90a" "target-features"="+16-bit-insts,+atomic-buffer-global-pk-add-f16-insts,+atomic-fadd-rtn-insts,+ci-insts,+cumode,+dl-insts,+dot1-insts,+dot10-insts,+dot2-insts,+dot3-insts,+dot4-insts,+dot5-insts,+dot6-insts,+dot7-insts,+dpp,+gfx8-insts,+gfx9-insts,+gfx90a-insts,+mai-insts,+s-memrealtime,+s-memtime-inst,+sramecc,+wavefrontsize64,-xnack" }
+        attributes #1 = { nocallback nofree nosync nounwind speculatable willreturn memory(none) }
+
+        !0 = !{i32 0, i32 1024}
+        !1 = !{}
+        !2 = !{!3, !3, i64 0}
+        !3 = !{!"float", !4, i64 0}
+        !4 = !{!"omnipotent char", !5, i64 0}
+        !5 = !{!"Simple C++ TBAA"}
+        """
+    )
+
+    print(link_modules([main_llvm_ir, dep_llvm_ir]).decode("utf-8"))
