93036378415ff10941ed3c12d21bf7a40b0af26a
Dominic Charrier <docharri@amd.com>
2024-03-25 12:28:47 +0100

    docs(*hip/target.py): use TODO(HIP/AMD) marker, clarify TODO

--- packages/numba-hip/numba/hip/target.py
+++ packages/numba-hip/numba/hip/target.py
@@ -68,7 +68,7 @@ from numba.hip import typing_lowering
 # -----------------------------------------------------------------------------
 # Typing
 
-# TODO(HIP) must be reloaded in case C++ extensions are added
+# TODO(HIP/AMD): Must be reloaded in case C++ extensions (experimental Numba-HIP-only feature under development) are added.
 module_hip_attributes = {}
 module_hip_attributes.update(typing_lowering.hipdevicelib.thestubs)
 module_hip_attributes.update(typing_lowering.hip.thestubs)
