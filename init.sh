ae2bba3af364b9511d6beac2c1ce97e87db4caad
Dominic Charrier <dominic.charrier@googlemail.com>
2024-07-23 11:40:29 +0200

    init.sh: always check out ci files from remote

--- init.sh
+++ init.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
-MAIN_BRANCH=${MAIN_BRANCH:-"origin/new_main"}
+# MIT License
+#
+# Copyright (c) 2023-2024 Advanced Micro Devices, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#!/usr/bin/env bash
+CI_BRANCH=${CI_BRANCH:-"new_main"}
 git fetch origin
-git checkout ${MAIN_BRANCH} -- ci
-git checkout ${MAIN_BRANCH} -- build_hip_python_pkgs.sh
-git checkout ${MAIN_BRANCH} -- _render_update_version.py
-git checkout ${MAIN_BRANCH} -- docs
-git checkout ${MAIN_BRANCH} -- examples
+git checkout origin/${CI_BRANCH} -- ci
+git checkout origin/${CI_BRANCH} -- build_hip_python_pkgs.sh
+git checkout origin/${CI_BRANCH} -- _render_update_version.py
+git checkout origin/${CI_BRANCH} -- docs
+git checkout origin/${CI_BRANCH} -- examples
 # unstage
 git reset -q HEAD ci build_hip_python_pkgs.sh _render_update_version.py docs examples
