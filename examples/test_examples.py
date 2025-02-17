51385cdc4f4d8102bf3c439afd22f32f5db1ab7b
domcharrier <docharri@amd.com>
2025-02-17 15:13:30 +0000

    + examples

---
+++ examples/test_examples.py
@@ -0,0 +1,96 @@
+# MIT License
+#
+# Copyright (c) 2023-2024 Advanced Micro Devices, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import runpy
+import shlex
+import subprocess
+
+import pytest
+from hip import ROCM_VERSION_TUPLE
+from hip import hip as hiprt
+
+device_printf_works = ROCM_VERSION_TUPLE[0:2] != (5, 5)
+
+props = hiprt.hipDeviceProp_t()
+hiprt.hipGetDeviceProperties(props, 0)
+gpugen = props.gcnArchName.decode("utf-8").split(":")[0]
+have_compatible_gpu_target = gpugen == "gfx90a"
+
+try:
+    from cuda import cuda
+
+    del cuda
+    have_hip_python_as_cuda = True
+except ImportError:
+    have_hip_python_as_cuda = False
+
+python_examples = [
+    "0_Basic_Usage/hip_deviceattributes.py",
+    "0_Basic_Usage/hip_deviceproperties.py",
+    "0_Basic_Usage/hip_python_device_array.py",
+    "0_Basic_Usage/hip_stream.py",
+    "0_Basic_Usage/hipblas_with_numpy.py",
+    "0_Basic_Usage/hipblas_with_numpy_and_cu_mask.py",
+    "0_Basic_Usage/hipfft.py",
+    "0_Basic_Usage/hiprand_monte_carlo_pi.py",
+    "0_Basic_Usage/rccl_comminitall_bcast.py",
+]
+
+if device_printf_works:
+    python_examples += [
+        "0_Basic_Usage/hiprtc_launch_kernel_args.py",
+        "0_Basic_Usage/hiprtc_launch_kernel_no_args.py",
+    ]
+
+if have_hip_python_as_cuda:
+    python_examples += [
+        "1_CUDA_Interop/cuda_stream.py",
+        "1_CUDA_Interop/cuda_error_hallucinate_enums.py",
+    ]
+
+python_examples += [
+    "2_Advanced/hip_jacobi.py",
+    "2_Advanced/hiprtc_linking_device_functions.py",
+]
+
+if have_compatible_gpu_target:
+    python_examples += [
+        "2_Advanced/hiprtc_jit_with_llvm_ir.py",
+        "2_Advanced/hiprtc_linking_llvm_ir.py",
+    ]
+
+
+@pytest.mark.parametrize("example", python_examples)
+def test_python_examples(example):
+    abspath = os.path.join(os.path.dirname(__file__), example)
+    runpy.run_path(abspath)
+
+
+if have_hip_python_as_cuda:
+
+    @pytest.mark.parametrize("example", ["1_CUDA_Interop/ccuda_stream.pyx"])
+    def test_cython_examples(example):
+        abspath = os.path.join(
+            os.path.dirname(__file__), os.path.dirname(example)
+        )
+        subprocess.check_call(shlex.split(f"make -C {abspath} run"))
