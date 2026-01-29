dcfdf1b74ceac2af75f3cfa49072c826aa4f0593
Philipp Samfass <philipp.samfass@amd.com>
2026-01-29 05:26:40 -0500

    fix: Add ROCm 7.2 data layout to test_amdgcn
    Support new data layout format with extended p8 address space specification (p8:128:128:128:48)

--- packages/numba-hip/numba/hip/tests/util/test_amdgcn.py
+++ packages/numba-hip/numba/hip/tests/util/test_amdgcn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S python3 -m pytest -v -s
 # MIT License
 #
-# Copyright (C) 2023-2024 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2023-2026 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -39,6 +39,7 @@ def test_00_print_datalayout():
         "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7",
         "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-p7:160:256:256:32-p8:128:128-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7:8",
         "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-p7:160:256:256:32-p8:128:128-p9:192:256:256:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7:8:9",  # ROCm 6.2.0
+        "e-p:64:64-p1:64:64-p2:32:32-p3:32:32-p4:64:64-p5:32:32-p6:32:32-p7:160:256:256:32-p8:128:128:128:48-p9:192:256:256:32-i64:64-v16:16-v24:32-v32:32-v48:64-v96:128-v192:256-v256:256-v512:512-v1024:1024-v2048:2048-n32:64-S32-A5-G1-ni:7:8:9",  # ROCm 7.2
     )
 
 
@@ -46,10 +47,10 @@ def test_01_verify_module():
     import faulthandler
 
     try:
-      faulthandler.enable()
+        faulthandler.enable()
     except Exception as e:
-      msg = "Failed to enable faulthandler due to:\n{err}"
-      warnings.warn(msg.format(err=e))
+        msg = "Failed to enable faulthandler due to:\n{err}"
+        warnings.warn(msg.format(err=e))
     machine = AMDGPUTargetMachine(target_cpu="gfx90a")
 
     dep_llvm_ir = textwrap.dedent(
